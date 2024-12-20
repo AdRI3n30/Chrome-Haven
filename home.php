4fccce9a3b521a290cd1338bb334c7c351422b73
TomBallauri <144777722+TomBallauri@users.noreply.github.com>
2024-12-20 17:43:14 +0100

    tom css

---
+++ home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="static/home.css">
+    <div class="display">
+        <div class="Header">
+            <div class=Header-Left>
+                <img src="image_url" alt="image_description">
+                <button>sexe</button>
+            </div> 
+            <div class="Header-Right">
+                <button>sexe</button>
+                <button>sexe</button>
+                <button>sexe</button>
+                <button>sexe</button>
+
+            </div>
+            <div class=""></div>
+        </div>
+    </div>
+
+</head>
+<body>
+    
+</body>
+</html>
